230682d604e4ba82ed94056dbe719500a3b887db
dmitriy.litichevskiy <litichevskiydv@gmail.com>
2018-10-17 00:00:08 +0500

    HashMap implementation and tests were refactored

--- src/hash-map.js
+++ src/hash-map.js
@@ -1,78 +1,78 @@
-module.exports = class HashMap{
-    constructor(equalityComparer){
-        this._equalityComparer = equalityComparer;
-        this._data = new Map();
+module.exports = class HashMap {
+  constructor(equalityComparer) {
+    this._equalityComparer = equalityComparer;
+    this._data = new Map();
+  }
+
+  get(key) {
+    let bucket = this._data.get(this._equalityComparer.getHashCode(key));
+    if (!bucket) return bucket;
+
+    let pair = bucket.find(x => this._equalityComparer.equals(x[0], key));
+    if (!pair) return pair;
+
+    return pair[1];
+  }
+
+  set(key, value) {
+    let hashCode = this._equalityComparer.getHashCode(key);
+    let bucket = this._data.get(hashCode);
+    if (!bucket) {
+      bucket = [];
+      this._data.set(hashCode, bucket);
     }
 
-    get(key){
-        let bucket = this._data.get(this._equalityComparer.getHashCode(key));
-        if(bucket === undefined) return bucket;
+    let pair = bucket.find(x => this._equalityComparer.equals(x[0], key));
+    if (!pair) bucket.push([key, value]);
+    else pair[1] = value;
 
-        let pair = bucket.find(x => this._equalityComparer.equals(x[0], key));
-        if(pair === undefined) return pair;
+    return this;
+  }
 
-        return pair[1];
+  has(key) {
+    return this.get(key) !== undefined;
+  }
+
+  delete(key) {
+    let bucket = this._data.get(this._equalityComparer.getHashCode(key));
+    if (!bucket) return false;
+
+    for (var i = 0; i < bucket.length && this._equalityComparer.equals(bucket[i][0], key) === false; i++);
+    if (i === bucket.length) return false;
+
+    bucket.splice(i, 1);
+    return true;
+  }
+
+  clear() {
+    this._data.clear();
+  }
+
+  forEach(callbackfn) {
+    this._data.forEach(bucket => bucket.forEach(value => callbackfn(value[1], value[0], this)));
+  }
+
+  *_iterateElements(selector) {
+    for (let bucket of this._data.values()) {
+      for (let pair of bucket) {
+        yield selector(pair);
+      }
     }
+  }
 
-    set(key, value){
-        let hashCode = this._equalityComparer.getHashCode(key);
-        let bucket = this._data.get(hashCode);
-        if(bucket === undefined){
-            bucket = [];
-            this._data.set(hashCode, bucket);
-        }
+  keys() {
+    return this._iterateElements(pair => pair[0]);
+  }
 
-        let pair = bucket.find(x => this._equalityComparer.equals(x[0], key));
-        if(pair === undefined) bucket.push([key, value]);
-        else pair[1] = value;
+  values() {
+    return this._iterateElements(pair => pair[1]);
+  }
 
-        return this;
-    }
+  entries() {
+    return this._iterateElements(pair => pair);
+  }
 
-    has(key){
-        return this.get(key) !== undefined;
-    }
-
-    delete(key){
-        let bucket = this._data.get(this._equalityComparer.getHashCode(key));
-        if(bucket === undefined) return false;
-
-        for(var i = 0; i < bucket.length && this._equalityComparer.equals(bucket[i][0], key) === false; i++);
-        if(i === bucket.length) return false;
-
-        bucket.splice(i, 1);
-        return true;
-    }
-
-    clear(){
-        this._data.clear();
-    }
-
-    forEach(callbackfn){
-        this._data.forEach(bucket => bucket.forEach(value => callbackfn(value[1], value[0], this)));
-    }
-
-    *_iterateElements(selector){
-        for(let bucket of this._data.values())
-        {
-            for(let pair of bucket){
-                yield selector(pair);                
-            }
-        }
-    }
-
-    keys(){
-        return this._iterateElements(pair => pair[0]);
-    }
-
-    values(){
-        return this._iterateElements(pair => pair[1]);
-    }
-
-    entries(){
-        return this._iterateElements(pair => pair);
-    }
-    [Symbol.iterator](){
-        return this.entries();
-    }
-}
+  [Symbol.iterator]() {
+    return this.entries();
+  }
+};
