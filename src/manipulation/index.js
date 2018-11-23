4700165e96ebb91ece22d9e81537ac1faa80e763
dmitriy.litichevskiy <litichevskiydv@gmail.com>
2018-11-23 08:48:45 +0500

    GroupBy iterator was implemented

--- src/manipulation/index.js
+++ src/manipulation/index.js
@@ -5,5 +5,6 @@ require("./iterators/concatIterator");
 require("./iterators/unionIterator");
 require("./iterators/exceptIterator");
 require("./iterators/distinctIterator");
+require("./iterators/groupByIterator");
 
 module.exports = require("./manipula");
---
+++ src/manipulation/iterators/groupByIterator.js
@@ -0,0 +1,41 @@
+require("./fromIterator");
+require("./selectIterator");
+
+const HashMap = require("../../collections/hashMap");
+let Manipula = require("../manipula");
+
+class GroupByIterator extends Manipula {
+  constructor(source, keySelector, elementSelector, comparer) {
+    super();
+    this._source = source;
+    this._keySelector = keySelector;
+    this._elementSelector = elementSelector;
+    this._comparer = comparer;
+  }
+
+  *[Symbol.iterator]() {
+    let map = !this._comparer ? new Map() : new HashMap(this._comparer);
+    for (const element of this._source) {
+      const elementKey = this._keySelector(element);
+      let bucket = map.get(elementKey);
+      if (!bucket) {
+        bucket = [];
+        map.set(elementKey, bucket);
+      }
+
+      bucket.push(element);
+    }
+
+    for (const pair of map) {
+      let values = Manipula.from(pair[1]);
+      if (this._elementSelector) values = values.select(this._elementSelector);
+      Object.defineProperty(values, "key", { value: pair[0] });
+
+      yield values;
+    }
+  }
+}
+
+Manipula.prototype.groupBy = function(options) {
+  return new GroupByIterator(this, options.keySelector, options.elementSelector, options.comparer);
+};
