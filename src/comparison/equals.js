351d67651d78e1cb906b6819b3d4f501715b8b5d
dmitriy.litichevskiy <litichevskiydv@gmail.com>
2019-01-02 22:59:52 +0500

    Base functionality of equals was implemented

---
+++ src/comparison/equals.js
@@ -0,0 +1,57 @@
+function equalsForIterables(first, second, options) {
+  const firstArray = Array.from(first);
+  const secondArray = Array.from(second);
+  if (firstArray.length !== secondArray.length) return false;
+
+  let usedMask = Array(secondArray.length).fill(false);
+  for (let i = 0; i < firstArray.length; i++) {
+    const leftBound = options.ignoreCollectionOrder ? 0 : i;
+    const rightBound = options.ignoreCollectionOrder ? secondArray.length : i + 1;
+    for (var j = leftBound; j < rightBound; j++)
+      if (!usedMask[j] && equals(firstArray[i], secondArray[j], options)) {
+        usedMask[j] = true;
+        break;
+      }
+
+    if (j === rightBound) return false;
+  }
+
+  return true;
+}
+
+function equalsForVariousObjects(first, second, options) {
+  const firstEquals = first["equals"];
+  const secondEquals = second["equals"];
+  if (firstEquals && secondEquals) return first.equals(second) && second.equals(first);
+  if (firstEquals) return first.equals(second);
+  if (secondEquals) return second.equals(first);
+
+  const firstConstructor = first.constructor;
+  const secondConstructor = second.constructor;
+  if (!options.ignoreObjectTypes && firstConstructor !== secondConstructor) return false;
+
+  const firstKeys = Object.keys(first).sort();
+  const secondKeys = Object.keys(second).sort();
+  if (firstKeys.length !== secondKeys.length) return false;
+  for (let i = 0; i < firstKeys.length; i++) if (firstKeys[i] !== secondKeys[i]) return false;
+  for (const key of firstKeys) if (equals(first[key], second[key], options) === false) return false;
+
+  return true;
+}
+
+function equals(first, second, options) {
+  const opts = options || {};
+
+  if (first === second) return true;
+
+  const firstType = typeof first;
+  if (firstType !== typeof second || firstType !== "object") return false;
+
+  if (first instanceof Date && second instanceof Date) return first.getTime() === second.getTime();
+
+  if (first[Symbol.iterator] && second[Symbol.iterator]) return equalsForIterables(first, second, opts);
+
+  return equalsForVariousObjects(first, second, opts);
+}
+
+module.exports.equals = equals;
---
+++ tests/comparison/equals.test.js
@@ -0,0 +1,317 @@
+const { equals } = require("../../src/comparison/equals");
+
+class Key {
+  constructor(hi, lo) {
+    this.hi = hi;
+    this.lo = lo;
+  }
+}
+
+class OtherKey {
+  constructor(hi, lo) {
+    this.hi = hi;
+    this.lo = lo;
+  }
+}
+
+describe("Should test equals", () => {
+  const otherFunction = x => x;
+  const differentFunction = x => x;
+
+  const testCases = [
+    {
+      toString: () => "Number and String mustn't be equal",
+      first: 1,
+      second: "1",
+      expected: false
+    },
+    {
+      toString: () => "Number and Boolean mustn't be equal",
+      first: 1,
+      second: true,
+      expected: false
+    },
+
+    {
+      toString: () => "Number and Null mustn't be equal",
+      first: 1,
+      second: null,
+      expected: false
+    },
+    {
+      toString: () => "Number and Undefined mustn't be equal",
+      first: 1,
+      second: undefined,
+      expected: false
+    },
+    {
+      toString: () => "Number and Object mustn't be equal",
+      first: 1,
+      second: { value: 1 },
+      expected: false
+    },
+    {
+      toString: () => "Number and Function mustn't be equal",
+      first: 1,
+      second: otherFunction,
+      expected: false
+    },
+    {
+      toString: () => "Must determine the equality of nulls",
+      first: null,
+      second: null,
+      expected: true
+    },
+    {
+      toString: () => "Must determine the equality of undefineds",
+      first: undefined,
+      second: undefined,
+      expected: true
+    },
+    {
+      toString: () => "Must determine the equality of booleans",
+      first: false,
+      second: false,
+      expected: true
+    },
+    {
+      toString: () => "Must determine the equality of numbers",
+      first: 15,
+      second: 15,
+      expected: true
+    },
+    {
+      toString: () => "Must determine the equality of strings",
+      first: "test",
+      second: "test",
+      expected: true
+    },
+    {
+      toString: () => "Must determine the equality of functions",
+      first: otherFunction,
+      second: otherFunction,
+      expected: true
+    },
+    {
+      toString: () => "Must determine the equality of dates",
+      first: new Date(1387585278000),
+      second: new Date("Fri Dec 20 2013 16:21:18 GMT-0800 (PST)"),
+      expected: true
+    },
+    {
+      toString: () => "Must determine the inequality of booleans",
+      first: false,
+      second: true,
+      expected: false
+    },
+    {
+      toString: () => "Must determine the inequality of numbers",
+      first: 15,
+      second: 20,
+      expected: false
+    },
+    {
+      toString: () => "Must determine the inequality of strings",
+      first: "test1",
+      second: "test2",
+      expected: false
+    },
+    {
+      toString: () => "Must determine the inequality of functions",
+      first: otherFunction,
+      second: differentFunction,
+      expected: false
+    },
+    {
+      toString: () => "Must determine the inequality of dates",
+      first: new Date(1387585278000),
+      second: new Date("Fri Dec 20 2000 16:21:18 GMT-0800 (PST)"),
+      expected: false
+    },
+    {
+      toString: () => "Must determine the equality of arrays of numbers",
+      first: [1, 2, 3],
+      second: [1, 2, 3],
+      expected: true
+    },
+    {
+      toString: () => "Must determine the equality of array and arguments of numbers",
+      first: [1, 2, 3],
+      second: (function() {
+        return arguments;
+      })(1, 2, 3),
+      expected: true
+    },
+    {
+      toString: () => "Must determine the inequality of arrays of numbers",
+      first: [1, 2, 3],
+      second: [1, 5, 3],
+      expected: false
+    },
+    {
+      toString: () => "Must determine the equality of array and arguments of numbers",
+      first: [1, 2, 3],
+      second: (function() {
+        return arguments;
+      })(1, 2, 4),
+      expected: false
+    },
+    {
+      toString: () => "Must determine the equality of arrays of numbers ignoring order",
+      first: [3, 2, 1],
+      second: [1, 2, 3],
+      options: { ignoreCollectionOrder: true },
+      expected: true
+    },
+    {
+      toString: () => "Must determine the inequality of arrays of numbers ignoring order",
+      first: [1, 1, 2, 2, 1],
+      second: [1, 1, 2, 2, 2],
+      options: { ignoreCollectionOrder: true },
+      expected: false
+    },
+    {
+      toString: () => "Must determine the inequality of array and various object",
+      first: [1, 2, 3],
+      second: { value: 1 },
+      expected: false
+    },
+    {
+      toString: () => "Must determine the equality of objects by checking both equalses",
+      first: {
+        a: 1,
+        b: 2,
+        equals: function(other) {
+          return this.a === other.a && this.b === other.b;
+        }
+      },
+      second: {
+        a: 1,
+        b: 2,
+        equals: function(other) {
+          return this.a === other.a && this.b === other.b;
+        }
+      },
+      expected: true
+    },
+    {
+      toString: () => "Must determine the inequality of objects by checking both equalses",
+      first: {
+        a: 1,
+        b: 2,
+        equals: function(other) {
+          return this.a === other.a && this.b === other.b;
+        }
+      },
+      second: {
+        a: 1,
+        b: 2,
+        c: 3,
+        equals: function(other) {
+          return this.a === other.a && this.b === other.b && this.c === other.c;
+        }
+      },
+      expected: false
+    },
+    {
+      toString: () => "Must determine the equality of objects by checking equals defined in first object",
+      first: {
+        a: 1,
+        b: 2,
+        equals: function(other) {
+          return this.a === other.a && this.b === other.b;
+        }
+      },
+      second: { a: 1, b: 2, c: 3 },
+      expected: true
+    },
+    {
+      toString: () => "Must determine the inequality of objects by checking equals defined in first object",
+      first: {
+        a: 1,
+        b: 2,
+        equals: function(other) {
+          return this.a === other.a && this.b === other.b;
+        }
+      },
+      second: { a: 2, b: 2, c: 3 },
+      expected: false
+    },
+    {
+      toString: () => "Must determine the equality of objects by checking equals defined in second object",
+      first: { a: 1, b: 2, c: 3 },
+      second: {
+        a: 1,
+        b: 2,
+        equals: function(other) {
+          return this.a === other.a && this.b === other.b;
+        }
+      },
+      expected: true
+    },
+    {
+      toString: () => "Must determine the inequality of objects by checking equals defined in second object",
+      first: { a: 2, b: 2, c: 3 },
+      second: {
+        a: 1,
+        b: 2,
+        equals: function(other) {
+          return this.a === other.a && this.b === other.b;
+        }
+      },
+      expected: false
+    },
+    {
+      toString: () => "Must determine the inequality of objects by comparing keys names",
+      first: { a: 2, b: 2 },
+      second: { b: 2, d: 2 },
+      expected: false
+    },
+    {
+      toString: () => "Must determine the equality of objects by deep comparing keys values",
+      first: { a: { id: 123, name: "test" }, b: { id: 345, description: "text" } },
+      second: { a: { id: 123, name: "test" }, b: { id: 345, description: "text" } },
+      expected: true
+    },
+    {
+      toString: () => "Must determine the inequality of objects by deep comparing keys values",
+      first: { a: { id: 123, name: "test1" }, b: { id: 345, description: "text2" } },
+      second: { a: { id: 123, name: "test2" }, b: { id: 345, description: "text1" } },
+      expected: false
+    },
+    {
+      toString: () => "Must determine the equality of arrays of complex type",
+      first: [new Key(1, 1), new Key(2, 2), new Key(3, 3)],
+      second: [new Key(1, 1), new Key(2, 2), new Key(3, 3)],
+      expected: true
+    },
+    {
+      toString: () => "Must determine the inequality of complex types to anonymous object with same structure",
+      first: new Key(1, 1),
+      second: { hi: 1, lo: 1 },
+      expected: false
+    },
+    {
+      toString: () =>
+        "Must determine the equality of complex type object to anonymous object with same structure if comparison of constructors was ignored",
+      first: new Key(1, 1),
+      second: { hi: 1, lo: 1 },
+      options: { ignoreObjectTypes: true },
+      expected: true
+    },
+    {
+      toString: () => "Must determine the inequality of complex types with same structure and different constructors",
+      first: new Key(1, 1),
+      second: new OtherKey(1, 1),
+      expected: false
+    }
+  ];
+
+  test.each(testCases)("%s", testCase => {
+    // When
+    const actual = equals(testCase.first, testCase.second, testCase.options);
+
+    // Then
+    expect(actual).toBe(testCase.expected);
+  });
+});
