be3164cd86bd38f8aed624079fcefd91ca3fee3f
dmitriy.litichevskiy <litichevskiydv@gmail.com>
2019-01-10 00:48:26 +0500

    Implementation for asynchronous for was added

--- src/async-processing/asyncLoop.js
+++ src/async-processing/asyncLoop.js
@@ -12,3 +12,17 @@ module.exports.forEachAsync = async function(source, body, loopState) {
     forEachBody(source[Symbol.iterator](), body, loopState, resolve);
   });
 };
+
+function forBody(i, toExclusive, body, loopState, callback) {
+  if (i === toExclusive) callback(loopState);
+  else {
+    body(i, loopState);
+    setImmediate(forBody.bind(null, i + 1, toExclusive, body, loopState, callback));
+  }
+}
+
+module.exports.forAsync = async function(fromInclusive, toExclusive, body, loopState) {
+  return await new Promise(resolve => {
+    forBody(fromInclusive, toExclusive, body, loopState, resolve);
+  });
+};
--- tests/async-processing/asyncLoop.test.js
+++ tests/async-processing/asyncLoop.test.js
@@ -1,4 +1,4 @@
-const { forEachAsync } = require("../../src/async-processing/asyncLoop");
+const { forEachAsync, forAsync } = require("../../src/async-processing/asyncLoop");
 
 test("Must compute sum of numbers in array via async foreach", async () => {
   // Given
@@ -7,6 +7,18 @@ test("Must compute sum of numbers in array via async foreach", async () => {
 
   // When
   const finalState = await forEachAsync(array, (currentValue, loopState) => (loopState.sum += currentValue), { sum: 0 });
+
+  // Then
+  expect(finalState.sum).toBe(expectedSum);
+});
+
+test("Must compute sum of numbers in array via async for", async () => {
+  // Given
+  const array = [1, 2, 3, 4, 5];
+  const expectedSum = array.reduce((accumulator, currentValue) => accumulator + currentValue);
+
+  // When
+  const finalState = await forAsync(0, array.length, (i, loopState) => (loopState.sum += array[i]), { sum: 0 });
 
   // Then
   expect(finalState.sum).toBe(expectedSum);
