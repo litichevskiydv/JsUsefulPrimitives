dfc15c6ff819407f5ba16b5ae2e4bbadc7f62038
dmitriy.litichevskiy <litichevskiydv@gmail.com>
2019-03-27 00:44:30 +0500

    Generator structure was changed

--- src/grpc/complier/bin/protoc-gen-client.js
+++ src/grpc/complier/bin/protoc-gen-client.js
@@ -1,3 +1,53 @@
 #!/usr/bin/env node
 
-require("../clientGenerator");
+const bl = require("bl");
+const path = require("path");
+const { CodeGeneratorRequest, CodeGeneratorResponse } = require("google-protobuf/google/protobuf/compiler/plugin_pb");
+
+const clientGenerator = require("../clientGenerator");
+
+/**
+ * Adds new file to code generation response
+ * @param {CodeGeneratorResponse} response Code generation response
+ * @param {string} originalFileName Name of the original file
+ * @param {string} fileExtension Extension of the new file
+ * @param {string} content New file content
+ */
+const addFileToResponse = (response, originalFileName, fileExtension, content) => {
+  const file = new CodeGeneratorResponse.File();
+  file.setName(
+    path.join(
+      path.dirname(originalFileName),
+      `${path.basename(originalFileName, path.extname(originalFileName))}_pb_client.${fileExtension}`
+    )
+  );
+  file.setContent(content);
+  response.addFile(file);
+};
+
+(async () => {
+  const request = CodeGeneratorRequest.deserializeBinary(
+    new Uint8Array(
+      await new Promise((resolve, reject) => {
+        process.stdin.pipe(
+          bl((error, data) => {
+            if (error) reject(error);
+            else resolve(data);
+          })
+        );
+      })
+    )
+  );
+
+  const response = new CodeGeneratorResponse();
+  const filesToGenerate = new Set(request.getFileToGenerateList());
+  request.getProtoFileList().forEach(fileDescriptor => {
+    const fileName = fileDescriptor.getName();
+    if (filesToGenerate.has(fileName) === false || fileDescriptor.getServiceList().length === 0) return;
+
+    addFileToResponse(response, fileName, "js", clientGenerator.generateJs(fileDescriptor));
+    addFileToResponse(response, fileName, "d.ts", clientGenerator.generateTypings(fileDescriptor));
+  });
+
+  process.stdout.write(Buffer.from(response.serializeBinary()));
+})();
--- src/grpc/complier/clientGenerator.js
+++ src/grpc/complier/clientGenerator.js
@@ -1,49 +1,19 @@
-const bl = require("bl");
-const path = require("path");
-const { CodeGeneratorRequest, CodeGeneratorResponse } = require("google-protobuf/google/protobuf/compiler/plugin_pb");
+const { FileDescriptorProto } = require("google-protobuf/google/protobuf/descriptor_pb");
 
 /**
- * Adds new file to code generation response
- * @param {CodeGeneratorResponse} response Code generation response
- * @param {string} originalFileName Name of the original file
- * @param {string} fileExtension Extension of the new file
- * @param {string} content New file content
+ * Generates JavaScript code for client
+ * @param {FileDescriptorProto} fileDescriptor Descriptor for proto file
  */
-const addFileToResponse = (response, originalFileName, fileExtension, content) => {
-  const file = new CodeGeneratorResponse.File();
-  file.setName(
-    path.join(
-      path.dirname(originalFileName),
-      `${path.basename(originalFileName, path.extname(originalFileName))}_pb_client.${fileExtension}`
-    )
-  );
-  file.setContent(content);
-  response.addFile(file);
+const generateJs = fileDescriptor => {
+  return "abc";
 };
 
-(async () => {
-  const request = CodeGeneratorRequest.deserializeBinary(
-    new Uint8Array(
-      await new Promise((resolve, reject) => {
-        process.stdin.pipe(
-          bl((error, data) => {
-            if (error) reject(error);
-            else resolve(data);
-          })
-        );
-      })
-    )
-  );
+/**
+ * Generates typings for client
+ * @param {FileDescriptorProto} fileDescriptor Descriptor for proto file
+ */
+const generateTypings = fileDescriptor => {
+  return "abc";
+};
 
-  const response = new CodeGeneratorResponse();
-  const filesToGenerate = new Set(request.getFileToGenerateList());
-  request.getProtoFileList().forEach(fileDescriptor => {
-    const fileName = fileDescriptor.getName();
-    if (filesToGenerate.has(fileName) === false || fileDescriptor.getServiceList().length === 0) return;
-
-    addFileToResponse(response, fileName, "js", "test");
-    addFileToResponse(response, fileName, "d.ts", "test");
-  });
-
-  process.stdout.write(Buffer.from(response.serializeBinary()));
-})();
+module.exports = { generateJs, generateTypings };
