b1fd8e47549a8f1a5ed13e3773ed3b2982eae3a8
dmitriy.litichevskiy <litichevskiydv@gmail.com>
2019-03-28 01:29:32 +0500

    Unary methods were implemented in client

--- src/grpc/complier/clientGenerator.js
+++ src/grpc/complier/clientGenerator.js
@@ -1,6 +1,12 @@
+const path = require("path");
+const camelCase = require("camelcase");
 const { FileDescriptorProto } = require("google-protobuf/google/protobuf/descriptor_pb");
 
 const MessagesCatalog = require("./messagesCatalog");
+const StringBuilder = require("./stringBuilder");
+
+const importPathToRequirePath = (importPath, suffix) =>
+  `./${path.basename(importPath, path.extname(importPath))}_${suffix || "pb"}`;
 
 /**
  * Generates JavaScript code for client
@@ -8,7 +14,40 @@ const MessagesCatalog = require("./messagesCatalog");
  * @param {FileDescriptorProto} fileDescriptor Descriptor for proto file
  */
 const generateJs = (messagesCatalog, fileDescriptor) => {
-  return JSON.stringify(messagesCatalog);
+  const builder = new StringBuilder();
+
+  builder
+    .appendLine('const grpc = require("grpc");')
+    .appendLine('const grpcPromise = require("grpc-promise");')
+    .appendLine();
+
+  const requirePath = importPathToRequirePath(fileDescriptor.getName(), "grpc_pb");
+  const clientsList = fileDescriptor.getServiceList().map(x => `${x.getName()}Client: ${x.getName()}ClientRaw`);
+  builder.appendLine(`const { ${clientsList.join(", ")} } = require("${requirePath}");`).appendLine();
+
+  fileDescriptor.getServiceList().forEach(service => {
+    builder
+      .appendLine(`module.exports.${service.getName()}Client = class ${service.getName()}Client {`)
+      .appendLineIdented("constructor(address, credentials) {", 1)
+      .appendLineIdented(`this._client = new ${service.getName()}ClientRaw(address, credentials);`, 2)
+      .appendLineIdented("grpcPromise.promisifyAll(this._client);", 2)
+      .appendLineIdented("}", 1);
+
+    service.getMethodList().forEach(method => {
+      if (method.hasServerStreaming === true && method.hasClientStreaming === true) {
+      } else if (method.hasClientStreaming === true) {
+      } else if (method.hasServerStreaming === true) {
+      } else
+        builder
+          .appendLineIdented(`${camelCase(method.getName())}(message) {`, 1)
+          .appendLineIdented(`return this._client.${camelCase(method.getName())}().sendMessage(message);`, 2)
+          .appendLineIdented("}", 1);
+    });
+
+    builder.appendLine("};");
+  });
+
+  return builder.toString();
 };
 
 /**
