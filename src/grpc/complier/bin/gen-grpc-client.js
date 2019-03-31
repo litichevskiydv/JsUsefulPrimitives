f6730423eab841ed4e6c7497dfe47385adbac737
dmitriy.litichevskiy <litichevskiydv@gmail.com>
2019-03-31 15:20:00 +0500

    Libraries search was simplified

--- src/grpc/complier/bin/gen-grpc-client.js
+++ src/grpc/complier/bin/gen-grpc-client.js
@@ -3,20 +3,24 @@
 "use strict";
 
 const path = require("path");
-const find = require("find");
-const findNodeModules = require("find-node-modules");
-const npmRunPath = require("npm-run-path");
+const pathKey = require("path-key");
 const { execFile } = require("child_process");
+
+const env = { ...process.env };
+const pathKeyName = pathKey({ env });
+env[pathKeyName] = process.mainModule.paths
+  .map(x => path.join(x, "grpc-tools", "bin"))
+  .concat(process.mainModule.paths.map(x => path.join(x, ".bin")))
+  .concat(env[pathKeyName])
+  .join(path.delimiter);
 
 const exeExt = process.platform === "win32" ? ".exe" : "";
 const scriptExt = process.platform === "win32" ? ".cmd" : "";
-const modulesDirectory = findNodeModules({ relative: false })[0];
 
-const protocPath = find.fileSync(new RegExp(`protoc${exeExt}$`), path.join(modulesDirectory, "grpc-tools"))[0];
 const args = ["-I", path.resolve(__dirname, "..", "include")].concat(
   process.argv.slice(2).map(x => (x.startsWith("--client_out") ? x.replace("--client_out", `--client${scriptExt}_out`) : x))
 );
-const child_process = execFile(protocPath, args, { env: npmRunPath.env() }, error => {
+const child_process = execFile(`protoc${exeExt}`, args, { env }, error => {
   if (error) throw error;
 });
 
