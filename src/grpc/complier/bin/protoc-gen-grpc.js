c7ad91e43f96811d8955f293dc790b7c4e61f27b
dmitriy.litichevskiy <litichevskiydv@gmail.com>
2019-03-31 15:46:40 +0500

    gRPC plugin path search algorithm was changed

--- src/grpc/complier/bin/protoc-gen-grpc.js
+++ src/grpc/complier/bin/protoc-gen-grpc.js
@@ -3,24 +3,27 @@
 "use strict";
 
 const path = require("path");
-const { execFile } = require("child_process");
 const find = require("find");
 const slash = require("slash");
-const findNodeModules = require("find-node-modules");
+const pathKey = require("path-key");
+const { execFile } = require("child_process");
+
+const env = { ...process.env };
+const pathKeyName = pathKey({ env });
+env[pathKeyName] = process.mainModule.paths
+  .map(x => path.join(x, ".bin"))
+  .concat(env[pathKeyName])
+  .join(path.delimiter);
 
 const exeExt = process.platform === "win32" ? ".exe" : "";
-const packageDirectory = path.join(findNodeModules({ relative: false })[0], "grpc-tools");
-
-const protocPath = find.fileSync(new RegExp(`protoc${exeExt}$`), packageDirectory)[0];
-const pluginPath = find.fileSync(new RegExp(`grpc_node_plugin${exeExt}$`), packageDirectory)[0];
+const scriptExt = process.platform === "win32" ? ".cmd" : "";
 
 const includePath = path.resolve(__dirname, "..", "include");
-const includedProtos = find.fileSync(/\.proto$/, includePath).map(x => slash(path.relative(includePath, path.normalize(x))));
-const args = ["--plugin=protoc-gen-grpc=" + pluginPath, "-I", includePath].concat(process.argv.slice(2)).concat(includedProtos);
-const child_process = execFile(protocPath, args, function(error, stdout, stderr) {
-  if (error) {
-    throw error;
-  }
+const args = ["-I", includePath]
+  .concat(process.argv.slice(2))
+  .concat(find.fileSync(/\.proto$/, includePath).map(x => slash(path.relative(includePath, path.normalize(x)))));
+const child_process = execFile(`grpc_tools_node_protoc${scriptExt}`, args, { env }, error => {
+  if (error) throw error;
 });
 
 child_process.stdout.pipe(process.stdout);
