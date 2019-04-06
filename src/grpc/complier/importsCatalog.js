0766b5406c232b290b7dfdf4fb48c2fd52774d8d
dmitriy.litichevskiy <litichevskiydv@gmail.com>
2019-04-06 13:05:22 +0500

    Enums from imported files were collected

--- src/grpc/complier/importsCatalog.js
+++ src/grpc/complier/importsCatalog.js
@@ -1,4 +1,4 @@
-const { FileDescriptorProto, DescriptorProto } = require("google-protobuf/google/protobuf/descriptor_pb");
+const { FileDescriptorProto, DescriptorProto, EnumDescriptorProto } = require("google-protobuf/google/protobuf/descriptor_pb");
 
 module.exports = class ImportsCatalog {
   constructor() {
@@ -6,6 +6,25 @@ module.exports = class ImportsCatalog {
     this.importedFiles = [];
     /**  @type {{ [fullName: string]: MessageInfo }} */
     this._messagesByFullNames = {};
+    /**  @type {{ [fullName: string]: EnumInfo }} */
+    this._enumsByFullNames = {};
+  }
+
+  /**
+   * @param {string} fullName
+   * @returns {string}
+   */
+  _prepareTypeFullName(fullName) {
+    return fullName.startsWith(".") ? fullName : `.${fullName}`;
+  }
+
+  /**
+   * @param {string} packageName
+   * @param {string} fullName
+   * @returns {string}
+   */
+  _prepareTypeName(packageName, fullName) {
+    return fullName.replace(packageName, "");
   }
 
   /**
@@ -16,12 +35,33 @@ module.exports = class ImportsCatalog {
    */
   _addMessage(fileDescriptor, namespace, messageDescriptor) {
     const fullName = `${namespace}.${messageDescriptor.getName()}`;
-    this._messagesByFullNames[fullName.startsWith(".") ? fullName : `.${fullName}`] = {
+
+    this._messagesByFullNames[this._prepareTypeFullName(fullName)] = {
       fileName: fileDescriptor.getName(),
-      name: fullName.replace(fileDescriptor.getPackage(), "")
+      name: this._prepareTypeName(fileDescriptor.getPackage(), fullName),
+      descriptor: messageDescriptor
     };
 
+    this._addEnums(fileDescriptor, fullName, messageDescriptor.getEnumTypeList());
     messageDescriptor.getNestedTypeList().forEach(x => this._addMessage(fileDescriptor, fullName, x));
+  }
+
+  /**
+   * Adds enums to catalog
+   * @param {FileDescriptorProto} fileDescriptor Proto file descriptor
+   * @param {string} namespace Enums namespace
+   * @param {Array<EnumDescriptorProto>} enumsDescriptors Enums descriptors
+   */
+  _addEnums(fileDescriptor, namespace, enumsDescriptors) {
+    enumsDescriptors.forEach(enumDescriptor => {
+      const fullName = `${namespace}.${enumDescriptor.getName()}`;
+
+      this._enumsByFullNames[this._prepareTypeFullName(fullName)] = {
+        fileName: fileDescriptor.getName(),
+        name: this._prepareTypeName(fileDescriptor.getPackage(), fullName),
+        descriptor: enumDescriptor
+      };
+    });
   }
 
   /**
@@ -30,6 +70,7 @@ module.exports = class ImportsCatalog {
    */
   processFileDescriptor(fileDescriptor) {
     this.importedFiles.push(fileDescriptor);
+    this._addEnums(fileDescriptor, fileDescriptor.getPackage(), fileDescriptor.getEnumTypeList());
     fileDescriptor.getMessageTypeList().forEach(x => this._addMessage(fileDescriptor, fileDescriptor.getPackage(), x));
   }
 
@@ -47,4 +88,12 @@ module.exports = class ImportsCatalog {
  * @typedef {Object} MessageInfo
  * @property {string} fileName Name of the file where message was declared
  * @property {string} name Name of the message
+ * @property {DescriptorProto} descriptor Message descriptor
+ */
+
+/**
+ * @typedef {Object} EnumInfo
+ * @property {string} fileName Name of the file where enum was declared
+ * @property {string} name Name of the enum
+ * @property {EnumDescriptorProto} descriptor Enum descriptor
  */
