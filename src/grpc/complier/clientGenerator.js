19e669fede606a667d8b35e5fe662fa1a2659210
dmitriy.litichevskiy <litichevskiydv@gmail.com>
2019-04-06 13:18:31 +0500

    Standard requires were moved to separate file

--- src/grpc/complier/clientGenerator.js
+++ src/grpc/complier/clientGenerator.js
@@ -6,6 +6,7 @@ const { FileDescriptorProto, ServiceDescriptorProto } = require("google-protobuf
 
 const ImportsCatalog = require("./importsCatalog");
 const StringBuilder = require("./stringBuilder");
+const standardRequires = require("./standardRequires");
 
 /**
  * @param {string} importPath
@@ -24,24 +25,10 @@ const importPathToNamespaceName = importPath =>
  */
 const importPathToRequirePath = (importPath, suffix) => `./${importPath.replace(path.extname(importPath), "")}_${suffix || "pb"}`;
 
-const predefinedPaths = new Map([
-  ["google/protobuf/compiler/plugin.proto", "google-protobuf/google/protobuf/compiler/plugin_pb"],
-  ["google/protobuf/any.proto", "google-protobuf/google/protobuf/any_pb"],
-  ["google/protobuf/api.proto", "google-protobuf/google/protobuf/api_pb"],
-  ["google/protobuf/descriptor.proto", "google-protobuf/google/protobuf/descriptor_pb"],
-  ["google/protobuf/duration.proto", "google-protobuf/google/protobuf/duration_pb"],
-  ["google/protobuf/empty.proto", "google-protobuf/google/protobuf/empty_pb"],
-  ["google/protobuf/field_mask.proto", "google-protobuf/google/protobuf/field_mask_pb"],
-  ["google/protobuf/source_context.proto", "google-protobuf/google/protobuf/source_context_pb"],
-  ["google/protobuf/struct.proto", "google-protobuf/google/protobuf/struct_pb"],
-  ["google/protobuf/timestamp.proto", "google-protobuf/google/protobuf/timestamp_pb"],
-  ["google/protobuf/type.proto", "google-protobuf/google/protobuf/type_pb"],
-  ["google/protobuf/wrappers.proto", "google-protobuf/google/protobuf/wrappers_pb"]
-]);
 /**
  * @param {string} importPath
  */
-const getRequirePath = importPath => predefinedPaths.get(slash(importPath)) || importPathToRequirePath(importPath);
+const getRequirePath = importPath => standardRequires.get(slash(importPath)) || importPathToRequirePath(importPath);
 
 /**
  *
