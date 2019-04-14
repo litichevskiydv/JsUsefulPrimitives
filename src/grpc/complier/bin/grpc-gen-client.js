4c01895096b84b1f670f76ae806d0441631b25d5
dmitriy.litichevskiy <litichevskiydv@gmail.com>
2019-04-15 02:12:46 +0500

    Output directory was created

--- src/grpc/complier/bin/grpc-gen-client.js
+++ src/grpc/complier/bin/grpc-gen-client.js
@@ -3,44 +3,61 @@
 "use strict";
 
 const path = require("path");
+const makeDir = require("make-dir");
 const pathKey = require("path-key");
 const { execFile } = require("child_process");
 
-const argv = require("yargs")
-  .usage("$0 [options] <protoFile>", "Produces clients for all services from given proto file")
-  .option("i", {
-    alias: "include",
-    type: "string",
-    array: true,
-    nargs: 1,
-    description: "Include directory"
-  })
-  .option("o", {
-    alias: "out",
-    type: "string",
-    default: process.cwd(),
-    description: "Output directory"
-  }).argv;
+/**
+ * @param {CommandLineArguments} args
+ */
+const createOutputDirectory = async args => await makeDir(args.out);
 
-const env = { ...process.env };
-const pathKeyName = pathKey({ env });
-env[pathKeyName] = process.mainModule.paths
-  .map(x => path.join(x, "grpc-tools", "bin"))
-  .concat(process.mainModule.paths.map(x => path.join(x, ".bin")))
-  .concat(env[pathKeyName])
-  .join(path.delimiter);
+(async () => {
+  const argv = require("yargs")
+    .usage("$0 [options] <protoFile>", "Produces clients for all services from given proto file")
+    .option("i", {
+      alias: "include",
+      type: "string",
+      array: true,
+      nargs: 1,
+      description: "Include directory"
+    })
+    .option("o", {
+      alias: "out",
+      type: "string",
+      default: process.cwd(),
+      description: "Output directory"
+    }).argv;
 
-const args = [
-  `--client${process.platform === "win32" ? ".cmd" : ""}_out`,
-  argv.out,
-  "-I",
-  path.resolve(__dirname, "..", "include"),
-  ...(argv.include || []).reduce((acc, cur) => acc.concat("-I", cur), []),
-  argv.protoFile
-];
-const child_process = execFile(`protoc${process.platform === "win32" ? ".exe" : ""}`, args, { env }, error => {
-  if (error) throw error;
-});
+  await createOutputDirectory(argv);
 
-child_process.stdout.pipe(process.stdout);
-child_process.stderr.pipe(process.stderr);
+  const env = { ...process.env };
+  const pathKeyName = pathKey({ env });
+  env[pathKeyName] = process.mainModule.paths
+    .map(x => path.join(x, "grpc-tools", "bin"))
+    .concat(process.mainModule.paths.map(x => path.join(x, ".bin")))
+    .concat(env[pathKeyName])
+    .join(path.delimiter);
+
+  const args = [
+    `--client${process.platform === "win32" ? ".cmd" : ""}_out`,
+    argv.out,
+    "-I",
+    path.resolve(__dirname, "..", "include"),
+    ...(argv.include || []).reduce((acc, cur) => acc.concat("-I", cur), []),
+    argv.protoFile
+  ];
+  const child_process = execFile(`protoc${process.platform === "win32" ? ".exe" : ""}`, args, { env }, error => {
+    if (error) throw error;
+  });
+
+  child_process.stdout.pipe(process.stdout);
+  child_process.stderr.pipe(process.stderr);
+})();
+
+/**
+ * @typedef {Object} CommandLineArguments
+ * @property {string} include
+ * @property {string} out
+ * @property {string} protoFile
+ */
