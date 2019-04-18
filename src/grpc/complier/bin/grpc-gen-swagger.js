8a930cf7f8f5486e41f2869c8a6d92164f3082df
dmitriy.litichevskiy <litichevskiydv@gmail.com>
2019-04-18 22:57:23 +0500

    Small refactoring was made

--- src/grpc/complier/bin/grpc-gen-swagger.js
+++ src/grpc/complier/bin/grpc-gen-swagger.js
@@ -4,7 +4,7 @@
 
 const path = require("path");
 const pathKey = require("path-key");
-const { execFile } = require("child_process");
+const { execFileSync } = require("child_process");
 
 const env = { ...process.env };
 const pathKeyName = pathKey({ env });
@@ -13,12 +13,5 @@ env[pathKeyName] = process.mainModule.paths
   .concat(__dirname, env[pathKeyName])
   .join(path.delimiter);
 
-const exeExt = process.platform === "win32" ? ".exe" : "";
-
-const args = ["-I", path.resolve(__dirname, "..", "include")].concat(process.argv.slice(2));
-const child_process = execFile(`protoc${exeExt}`, args, { env }, error => {
-  if (error) throw error;
-});
-
-child_process.stdout.pipe(process.stdout);
-child_process.stderr.pipe(process.stderr);
+const args = ["-I", path.join(__dirname, "..", "include")].concat(process.argv.slice(2));
+execFileSync(`protoc${process.platform === "win32" ? ".exe" : ""}`, args, { env });
