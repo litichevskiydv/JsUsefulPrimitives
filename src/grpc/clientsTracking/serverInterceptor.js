5b3f30c21936b2578fc29ff0c51a56f2e8f77bc6
dmitriy.litichevskiy <litichevskiydv@gmail.com>
2020-01-28 23:23:50 +0300

    Information about called method was captured

--- src/grpc/clientsTracking/serverInterceptor.js
+++ src/grpc/clientsTracking/serverInterceptor.js
@@ -1,8 +1,8 @@
 const { Counter } = require("prom-client");
 
-const grpcServerUsageTotal = new Counter({
-  name: "grpc_server_usage_total",
-  labelNames: ["consumer_name", "consumer_version", "client_version"],
+const grpcServerCallsTotal = new Counter({
+  name: "grpc_server_calls_total",
+  labelNames: ["consumer_name", "consumer_version", "client_version", "grpc_method", "grpc_service", "grpc_type"],
   help: "Total number of calls, made by various consumers."
 });
 
@@ -22,19 +22,35 @@ function getConsumerDescription(metadata) {
 }
 
 /**
+ * @param {string} path
+ * @returns {{serviceName: string, methodName: string}}
+ */
+const parseMethodPath = path => {
+  const [, serviceName, methodName] = path.split("/");
+  return { serviceName, methodName };
+};
+
+/**
+ * @param {import("grpc").MethodDefinition} methodDefinition
+ * @returns {"bidi" | "client_stream" | "server_stream" | "unary"}
+ */
+const getMethodType = methodDefinition => {
+  if (methodDefinition.requestStream) return methodDefinition.responseStream ? "bidi" : "client_stream";
+  return methodDefinition.responseStream ? "server_stream" : "unary";
+};
+
+/**
  * @param {import("grpc").ServerUnaryCall | import("grpc").ServerWritableStream | import("grpc").ServerReadableStream | import("grpc").ServerDuplexStream} call
  * @param {import("grpc").MethodDefinition} methodDefinition
  * @param {Function} next
  * @returns {Promise<any>}
  */
 module.exports = function(call, methodDefinition, next) {
-  const consumerDescription = getConsumerDescription(call.metadata);
-  grpcServerUsageTotal
-    .labels(
-      consumerDescription.consumerName || "unknown",
-      consumerDescription.consumerVersion || "unknown",
-      consumerDescription.clientVersion || "unknown"
-    )
+  const { consumerName, consumerVersion, clientVersion } = getConsumerDescription(call.metadata);
+  const { serviceName, methodName } = parseMethodPath(methodDefinition.path);
+  const methodType = getMethodType(methodDefinition);
+  grpcServerCallsTotal
+    .labels(consumerName || "unknown", consumerVersion || "unknown", clientVersion || "unknown", methodName, serviceName, methodType)
     .inc();
 
   return next(call);
